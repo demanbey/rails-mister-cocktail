d8c216e757c46e965748e17f75cd0af2b53a1ec4
Heather deManbey <h.demanbey@gmail.com>
2019-05-18 19:27:27 -0400

    rails new

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,19 +1,3 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
 import 'bootstrap';
 
 console.log('Hello World from Webpacker')
@@ -21,23 +5,20 @@ console.log('Hello World from Webpacker')
 
 
 // Wrap every letter in a span
-$('.ml2').each(function(){
+$('.ml10 .letters').each(function(){
   $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml2 .letter',
-    scale: [4,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 950,
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 1300,
     delay: function(el, i) {
-      return 70*i;
+      return 45 * i;
     }
   }).add({
-    targets: '.ml2',
+    targets: '.ml10',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
